3816a9ccf011e0f21925966641001e9b0f3348b9
Arthur Paixao <arthur.paixao@tre-ma.jus.br>
2024-10-11 11:31:14 -0300

    TEST: Adicionado teste para repository TarefaRepository

--- src/test/java/com/meuprojeto/meuapp/repository/TarefaRepositoryTest.java
+++ src/test/java/com/meuprojeto/meuapp/repository/TarefaRepositoryTest.java
@@ -1,5 +1,11 @@
 package com.meuprojeto.meuapp.repository;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.ObjectInputFilter.Status;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -16,13 +22,20 @@ public class TarefaRepositoryTest {
     @Autowired
     EntityManager entityManager;
 
+    @Autowired
+    private TarefaRepository tarefaRepository;
+
     @Test
     public void findByTarefa() {
+        Tarefa novaTarefa = new Tarefa();
+        novaTarefa.setTitulo("Testar tarefa");
+        novaTarefa.setStatus(Tarefa.StatusTarefa.EM_ANDAMENTO);
+        tarefaRepository.save(novaTarefa);
 
+        Optional<Tarefa> tarefaEncontrada = tarefaRepository.findByTarefa("Testar tarefa");
+
+        assertTrue(tarefaEncontrada.isPresent(), "A tarefa deveria ter sido encontrada");
+        assertEquals("Testar tarefa", tarefaEncontrada.get().getTitulo(), "O nome da tarefa não corresponde");
     }
-
-    // private Tarefa criarTarefa(Tarefa tarefa) {
-    // Tarefa newTarefa = new Tarefa(tarefa);
-    // }
 
 }
